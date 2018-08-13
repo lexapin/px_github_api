b39bff58441ce4c853be43dd3786892cb9133b8d
Aleksei Fomin <aleksei.fomin@arloid.com>
2018-08-13 18:10:10 +0300

    utils

---
+++ github_check/utils/time_utils.py
@@ -0,0 +1,29 @@
+# Преобразование строки времени в формат datetime
+# По условию задания нельзя использовать внешние библиотеки
+# Лучше конечно было обойтись python-dateutil
+
+import re, datetime
+
+class MyDateTime():
+  def __init__(self, date_like_str):
+    if not isinstance(date_like_str, str):
+      raise TypeError('Is not a string')
+    if not self.check_str_for_date(date_like_str):
+      raise TypeError('date format is invalid')
+    self.str_date = date_like_str
+    self.date = self.from_str()
+
+  def check_str_for_date(self, str_obj):
+    regex = re.compile(
+      r'^\d{4}\-\d{2}\-\d{2}T\d{2}\:\d{2}\:\d{2}Z', re.IGNORECASE
+    )
+    return re.match(regex, str_obj) is not None
+
+  def from_str(self):
+    return datetime.datetime.strptime(self.str_date[:-1].replace('T', ' '), '%Y-%m-%d %H:%M:%S')
+
+
+# if __name__ == '__main__':
+#   time = MyDateTime('2017-12-12T01:02:03Z')
+#   print(time.date.isoformat())
+#   # MyDateTime('2017-12-01')
